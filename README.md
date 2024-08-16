9899c5210986b2bc6c99e9f01b2075dc02dbf672
LucianoSant006 <santiagoluciano383@gmail.com>
2024-08-16 11:19:27 -0300

    Readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Java COMPLETO Programação Orientada a Objetos + Projetos
+
+## Apredendo sobre estrutura sequêncial
+
+div align="center"
+img https://github.com/LucianoSant006/Curso-JAVA/issues/3#issue-2470360691" width="0px" /
+/div
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -8,28 +8,16 @@ public class Main {
 		Locale.setDefault(Locale.US);
 
 		Scanner sc = new Scanner(System.in);
-		
-		double price = 400.00;
-		
-		//  ela estânciada ela pertece ao escopo public static void Main
-		double discount = 0;
-		
-		if (price < 200.00) {
-			discount = price *0.1;
-			// Quando a estrutura termina a execução as váriavs que tiverem sido declaradas
-			// aqui dentro da estrutura vão ser desalocadas da mémeoria
-		}
-		/* outra forma de estância
-		else { 
-			discount =0;
-		}*/
-		
-		System.out.println(discount);
-		
-		
-				
 
-		
+		int i = sc.nextInt();
+
+		if (i < 0) {
+			System.out.println("Negativo");
+		} else {
+			System.out.println("Não Negativo");
+
+		}
+
 	}
 
 }
