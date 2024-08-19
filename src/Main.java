7488289abdedd6d6eb49544347694555f65eab5b
LucianoSant006 <santiagoluciano383@gmail.com>
2024-08-19 20:43:24 -0300

    Exerc 02 Plano Cartesiano

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,28 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		int i = 0;
-		System.out.println("Digite a senha");
-		int senha = sc.nextInt();
-		while (senha != 2002) {
+		int x = 0;
+		int y = 0;
+		System.out.printf("Digite a cordenada X %n");
+		x = sc.nextInt();
+		System.out.printf("Digite a Cordenada Y %n");
+		y = sc.nextInt();
+		while (x != 0 && y != 0) {
 
-			System.out.println("Senha Invalida");
-			senha = sc.nextInt();
-
+			if (x > 0 && y > 0) {
+				System.out.println("primeiro");
+			} else if (x < 0 && y > 0) {
+				System.out.println("segundo");
+			} else if (x < 0 && y < 0) {
+				System.out.println("terceiro");
+			} else {
+				System.out.println("quarto");
+			}
+			System.out.printf("Digite a cordenada X %n");
+			x = sc.nextInt();
+			System.out.printf("Digite a Cordenada Y %n");
+			y = sc.nextInt();
 		}
-		System.out.println("Acesso Permitido");
 
 		sc.close();
 
