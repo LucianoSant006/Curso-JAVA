1a4a0e815ae2ed77d44c21c9450bfbaf7c4fd910
LucianoSant006 <santiagoluciano383@gmail.com>
2024-08-22 23:36:41 -0300

    Funções para String

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,49 @@
 import java.util.Scanner;
 
-public class Main{
-	
+public class Main {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
+		String original = "abcde FGHIJ ABC abc DEFG   ";
 		
-		System.out.println("Digite o numero de numero");
-		int N = sc.nextInt();
-		int f =0;
-		for(int x = N;x >=0 ;x--)
-		{
-				f = x *N;
-				System.out.println(f);
+		//Deixa em  minúsculo 
+		String s01 = original.toLowerCase();
 		
-				
-		}
+		//Deixa o valor da varial em Maiúsculo
+		String s02 = original.toUpperCase();
 		
+		//Tira os espaços deixados  na váriavel
+		String s03 = original.trim();
 		
-		sc.close();
+		//Começa  a conta apartir do índice 2 
+		String s04 = original.substring(2);
+		
+		//Começa a conta apartir do índice 2 e termina no 9
+		String s05 = original.substring(2, 9);
+		
+		//Substitui a pelo  x
+		String s06 = original.replace('a', 'x');
+		
+		String s07 = original.replace("abc", "xy");
+		
+		//Pega o primeiro indice
+		int i = original.indexOf("bc");
+		
+		//Obtem o último índice 
+		int j = original.lastIndexOf("bc");
+
+		System.out.println("Original: -" + original + "-");
+		System.out.println("toLowerCase: -" + s01 + "-");
+		System.out.println("toUpperCase: -" + s02 + "-");
+		System.out.println("Trim: -" + s03 + "-");
+		System.out.println("substring: -" + s04 + "-");
+		System.out.println("substring: -" + s05 + "-");
+		System.out.println("replace('a', 'x'):-" + s06 + "-");
+		System.out.println("replace('abc', 'xy'):-" + s07 + "-");
+		System.out.println("Index of 'bc': " + i);
+		System.out.println("Last index of 'bc': " + j);
 
 		
 	}
-}
-	
 
+}
