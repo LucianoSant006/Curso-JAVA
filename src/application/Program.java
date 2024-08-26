dd63d957fde842a2d97c11da01e9a9564e83e3ea
LucianoSant006 <santiagoluciano383@gmail.com>
2024-08-26 19:21:22 -0300

    Exer 03 Student

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,32 +3,35 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.Employee;
+import entities.Student;
+
+
 
 public class Program {
 
 	public static void main(String[] Args) {
-
+		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-
-		Employee employee = new Employee();
 		
-		double percentege;
+		Student stu= new Student();
 		
-		System.out.println("Wrote your name");
-		employee.Name = sc.next();
-		System.out.println("GrossSalary: ");
-		employee.GrossSalary = sc.nextDouble();
-		System.out.println("Tax: ");
-		employee.Tax = sc.nextDouble();
-		
-		System.out.println();
-		System.out.println("Employee: " + employee);
-		System.out.println();
-		System.out.println("Which percentage to increase salry?: ");
-		percentege = sc.nextDouble();
-		employee.increaseSalary(percentege);
-		System.out.println("Update data : " + employee);
+		System.out.println("What's your name");
+		stu.name = sc.next();
+		sc.nextLine();
+		System.out.println("write your grade for the three semesters ");
+		stu.nota1 = sc.nextDouble();
+		stu.nota2 =sc.nextDouble();
+		stu.nota3 = sc.nextDouble();
+		System.out.println(stu);
+		if (stu.Finalgrade() < 60.0) {
+		System.out.println("Failed");
+		System.out.printf("Missing %.2f Points %n", stu.calcPoints());
+		}
+		else {
+		System.out.println("PASS");
+		}
+		sc.close();
+	
 	}
 }
