b2d7ba57f54257bab98388574262c117e07ad11b
LucianoSant006 <santiagoluciano383@gmail.com>
2024-09-02 16:55:34 -0300

    Exerc 02 soma_vetor

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,36 +3,49 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.Product;
-
 public class Program {
 
 	public static void main(String[] Args) {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Quantos numeros vai digitar ?");
+		System.out.print("Quantos números você vai digitar? ");
 		int N = sc.nextInt();
+		double g = 0.0;
+		double[] vect = new double[N];
 
-		int[] vect = new int[N];
+		for (int i = 0; i < vect.length; i++) {
+
+			System.out.print("Digite um numero: ");
+			vect[i] = sc.nextDouble();
+			g += vect[i];
+
+		}
+
+		double sum = 0.0;
+		double soma = 0.0;
+
+		for (int i = 0; i < vect.length; i++) {
+
+			sum += vect[i];
+			soma += vect[i];
+
+		}
+		System.out.print("VALORES = ");
+		for (int i = 0; i < vect.length; i++) {
+
+			
+			System.out.printf("%.2f ",vect[i]);
+
+		}
 		
-		for(int i = 0; i < vect.length;i++) {
-			System.out.print("Digite um numeor");
-			 vect[i] = sc.nextInt();
-			 
-			
-		}
-		System.out.println("Numeros Negativos:");
-		for(int i=0; i< vect.length; i++) {
-	        if (vect[i] < 0) {
-	        	 
-	        	System.out.printf("%d\n", vect[i]);
-	        }
-	    
+		double avg = soma/ N;
+		
+		System.out.println();
+		System.out.printf("Soma =  %.2f %n" , soma);
+		System.out.printf("Média =  %.2f %n" , avg);
+	
 
-			
-		}
 		sc.close();
 	}
 }
-
